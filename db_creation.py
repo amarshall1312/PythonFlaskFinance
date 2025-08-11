9a3bb53a56ae29e6699bfeab34ad202014f81b5e
amarshall1312 <amarshall131299@gmail.com>
2025-08-11 22:30:55 +0100

    added full sql tables

--- db_creation.py
+++ db_creation.py
@@ -6,14 +6,42 @@ FINANCES_DB = "finance.db"
 
 if not os.path.exists(FINANCES_DB):
     connection = sqlite3.connect(FINANCES_DB)
-    connection.execute("""
+    connection.executescript("""
         CREATE TABLE savingpots (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
             type TEXT NOT NULL,
             target INTEGER,
             deadline INTEGER
-        )
+        );
+        CREATE TABLE outgoings (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            fixed INTEGER NOT NULL,
+            amount INTEGER NOT NULL,
+            frequency TEXT NOT NULL,
+            date INTEGER,
+            category TEXT
+        );
+        CREATE TABLE owners (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL
+        );
+        CREATE TABLE baseincomes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            owner_id INTEGER NOT NULL,
+            amount INTEGER NOT NULL,
+            frequency TEXT NOT NULL,
+            FOREIGN KEY (owner_id) REFERENCES owners(id)
+        );
+        CREATE TABLE additionalincomes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            owner_id INTEGER NOT NULL,
+            amount INTEGER NOT NULL,
+            frequency TEXT,
+            date date,
+            FOREIGN KEY (owner_id) REFERENCES owners(id)
+        );
     """)
     connection.commit()
     connection.close()
